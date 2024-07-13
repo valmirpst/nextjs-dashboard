fe13071a7687e721825d244e27b5a43e8628ae1f
Valmir Paiva Stachin <valmirpaivacpm@gmail.com>
2024-07-13 12:13:33 -0300

    done to chapter 12, until search and pagination

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,15 +11,12 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise(resolve => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -37,7 +34,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.rows.map((invoice) => ({
+    const latestInvoices = data.rows.map(invoice => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
@@ -84,10 +81,7 @@ export async function fetchCardData() {
 }
 
 const ITEMS_PER_PAGE = 6;
-export async function fetchFilteredInvoices(
-  query: string,
-  currentPage: number,
-) {
+export async function fetchFilteredInvoices(query: string, currentPage: number) {
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -152,7 +146,7 @@ export async function fetchInvoiceById(id: string) {
       WHERE invoices.id = ${id};
     `;
 
-    const invoice = data.rows.map((invoice) => ({
+    const invoice = data.rows.map(invoice => ({
       ...invoice,
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
@@ -203,7 +197,7 @@ export async function fetchFilteredCustomers(query: string) {
 		ORDER BY customers.name ASC
 	  `;
 
-    const customers = data.rows.map((customer) => ({
+    const customers = data.rows.map(customer => ({
       ...customer,
       total_pending: formatCurrency(customer.total_pending),
       total_paid: formatCurrency(customer.total_paid),
--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
-// import bcrypt from 'bcrypt';
 // import { db } from '@vercel/postgres';
-// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+// import bcrypt from 'bcrypt';
+// import { customers, invoices, revenue, users } from '../lib/placeholder-data';
 
 // const client = await db.connect();
 
@@ -16,14 +16,14 @@
 //   `;
 
 //   const insertedUsers = await Promise.all(
-//     users.map(async (user) => {
+//     users.map(async user => {
 //       const hashedPassword = await bcrypt.hash(user.password, 10);
 //       return client.sql`
 //         INSERT INTO users (id, name, email, password)
 //         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
 //         ON CONFLICT (id) DO NOTHING;
 //       `;
-//     }),
+//     })
 //   );
 
 //   return insertedUsers;
@@ -44,12 +44,12 @@
 
 //   const insertedInvoices = await Promise.all(
 //     invoices.map(
-//       (invoice) => client.sql`
+//       invoice => client.sql`
 //         INSERT INTO invoices (customer_id, amount, status, date)
 //         VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
 //         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
+//       `
+//     )
 //   );
 
 //   return insertedInvoices;
@@ -69,12 +69,12 @@
 
 //   const insertedCustomers = await Promise.all(
 //     customers.map(
-//       (customer) => client.sql`
+//       customer => client.sql`
 //         INSERT INTO customers (id, name, email, image_url)
 //         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
 //         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
+//       `
+//     )
 //   );
 
 //   return insertedCustomers;
@@ -90,33 +90,33 @@
 
 //   const insertedRevenue = await Promise.all(
 //     revenue.map(
-//       (rev) => client.sql`
+//       rev => client.sql`
 //         INSERT INTO revenue (month, revenue)
 //         VALUES (${rev.month}, ${rev.revenue})
 //         ON CONFLICT (month) DO NOTHING;
-//       `,
-//     ),
+//       `
+//     )
 //   );
 
 //   return insertedRevenue;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  //   await client.sql`BEGIN`;
-  //   await seedUsers();
-  //   await seedCustomers();
-  //   await seedInvoices();
-  //   await seedRevenue();
-  //   await client.sql`COMMIT`;
+// export async function GET() {
+//   //   return Response.json({
+//   //     message:
+//   //       'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//   // });
+//   try {
+//     await client.sql`BEGIN`;
+//     await seedUsers();
+//     await seedCustomers();
+//     await seedInvoices();
+//     await seedRevenue();
+//     await client.sql`COMMIT`;
 
-  //   return Response.json({ message: 'Database seeded successfully' });
-  // } catch (error) {
-  //   await client.sql`ROLLBACK`;
-  //   return Response.json({ error }, { status: 500 });
-  // }
-}
+//     return Response.json({ message: 'Database seeded successfully' });
+//   } catch (error) {
+//     await client.sql`ROLLBACK`;
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
